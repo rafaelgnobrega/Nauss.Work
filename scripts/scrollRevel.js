c3f763f6347b9b3698062f25a38f6ac412c28ed9
Rafael Nóbrega <90426277+rafaelgnobrega@users.noreply.github.com>
2022-12-18 19:54:40 -0300

    first commit

---
+++ scripts/scrollRevel.js
@@ -0,0 +1,13 @@
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('.link1', { duration: 1000, distance: '50px', origin: 'top' });
+sr.reveal('.link2', { duration: 1500, distance: '50px', origin: 'top' });
+sr.reveal('.link3', { duration: 2000, distance: '50px', origin: 'top' });
+
+ScrollReveal().reveal('.NaussHome', {
+    distance: '0px',
+    opacity: 0.08
+});
+
+sr.reveal('.imgStudio', { duration: 1000, distance: '50px', origin: 'right' });
+sr.reveal('.textStudio', { duration: 1500, distance: '50px', origin: 'left' });
