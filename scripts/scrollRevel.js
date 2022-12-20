2aabde60486101851afc86deff6233fb58b06067
Rafael Nóbrega <90426277+rafaelgnobrega@users.noreply.github.com>
2022-12-19 22:14:50 -0300

    Adicionando botão para subir para o topo

--- scripts/scrollRevel.js
+++ scripts/scrollRevel.js
@@ -13,3 +13,41 @@ sr.reveal('.imgStudio', { duration: 1000, distance: '50px', origin: 'right' });
 sr.reveal('.textStudio', { duration: 1500, distance: '50px', origin: 'left' });
 sr.reveal('.direita', { duration: 1000, distance: '50px', origin: 'right' });
 sr.reveal('.esquerda', { duration: 1500, distance: '50px', origin: 'left' });
+
+
+ // Obtém o elemento com o id "topo"
+ var botao = document.getElementById("topo");
+
+ // Quando o usuário clicar no botão, execute a função scrollToTop
+ botao.addEventListener("click", scrollToTop);
+
+ function scrollToTop() {
+   // Obtém o elemento html
+   var html = document.documentElement;
+   // Obtém o elemento body
+   var body = document.body;
+
+   // Scroll lento para o topo da página
+   window.scrollTo({
+     top: 0,
+     behavior: 'smooth'
+   });
+   
+ }
+
+ // Obtém o elemento com o id "topo"
+var botao = document.getElementById("topo");
+
+// Verifica se a página está no início
+function verificarPosicao() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    botao.style.display = "block"; // Exibe o botão
+  } else {
+    botao.style.display = "none"; // Esconde o botão
+  }
+}
+
+// Executa a função verificarPosicao quando a página é rolada
+window.onscroll = function() {
+  verificarPosicao();
+};
